5e1ed3fe4e2259a9cdcc0e4f024368b149723a8d
jung0072 <jung0072@algonquinlive.com>
2023-05-12 11:50:01 -0400

    add the pause and play feature

--- src/pages/PlayWithAudioContext.jsx
+++ src/pages/PlayWithAudioContext.jsx
@@ -20,7 +20,16 @@ const Play = () => {
     console.log("duration changed", ctx.playOption.duration);
   }, [ctx.playOption.duration]);
 
+  let startingTime;
+  const [totalDurationOfSourceNodes, setTotalDurationOfSourceNodes] =
+    React.useState(0);
   const [isPlaying, setIsPlaying] = React.useState(false);
+  const [isFirstPlaying, setIsFirstPlaying] = React.useState(true);
+  const [audioNodes, setAudioNodes] = React.useState([]);
+  const [audioBuffers, setAudioBuffers] = React.useState([]);
+  const [currentTrackNumber, setCurrentTrackNumber] = React.useState(0);
+  const [pausedAt, setPausedAt] = React.useState(0);
+  const [pausedBuffer, setPausedBuffer] = React.useState(null);
   const audioFiles = [
     "src/assets/one.wav",
     "src/assets/two.wav",
@@ -34,6 +43,8 @@ const Play = () => {
     "src/assets/ten.wav",
   ];
 
+  // const audioFiles = ["src/assets/mixdown.wav"];
+
   let audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
   // utility function to fetch and decode an audio file
@@ -45,16 +56,42 @@ const Play = () => {
   }
 
   // function to play a sequence of AudioBuffers
-  function playAudioBuffersInSequence(audioBuffers) {
-    let time = audioContext.currentTime;
-    for (let audioBuffer of audioBuffers) {
-      let source = audioContext.createBufferSource();
-      source.buffer = audioBuffer;
-      source.connect(audioContext.destination);
-      source.start(time);
-      time += audioBuffer.duration;
-    }
+  function connectNodesToDestination(audioBuffers) {
+    let sum = 0;
+    let nodes = audioBuffers.map((audioBuffer) => {
+      let node = audioContext.createBufferSource();
+      node.buffer = audioBuffer;
+      sum += audioBuffer.duration;
+      node.connect(audioContext.destination);
+      return node;
+    });
+    setAudioNodes(nodes);
+    setTotalDurationOfSourceNodes(sum);
   }
+
+  useEffect(() => {
+    audioNodes.forEach((node, index) => {
+      if (index < audioNodes.length - 1) {
+        node.onended = () => {
+          console.log("Track #", index, "ended");
+          audioNodes[index + 1].start(0);
+          setCurrentTrackNumber(index + 1);
+        };
+      } else {
+        node.onended = () => {
+          console.log("Last Track ended");
+          console.log(
+            "Average delay per track:",
+            ((Date.now() - startingTime) / 1000 - totalDurationOfSourceNodes) /
+              (audioNodes.length - 1),
+            "seconds"
+          );
+          setCurrentTrackNumber(0);
+          setIsPlaying(false);
+        };
+      }
+    });
+  }, [audioNodes]);
 
   useEffect(() => {
     // load all audio files, then play them in sequence
@@ -63,21 +100,60 @@ const Play = () => {
         ? audioFiles
         : audioFiles.filter((_, i) => i % 2 === 0);
     Promise.all(filesToLoad.map(loadAudioFile))
-      .then((audioBuffers) => playAudioBuffersInSequence(audioBuffers))
+      .then((audioBuffers) => {
+        setAudioBuffers(audioBuffers);
+        connectNodesToDestination(audioBuffers);
+      })
       .catch(console.error);
   }, [duration]);
 
   function handleControl() {
-    console.log("handleControl");
     if (isPlaying) {
-      audioContext.pause().then(() => {
-        console.log("Playback paused successfully");
-        setIsPlaying(!isPlaying);
-      });
+      console.log("PAUSE at track #", currentTrackNumber);
+      // Remove the onended event listener and stop the current source node
+      audioNodes[currentTrackNumber].onended = null;
+      audioNodes[currentTrackNumber].stop();
+      // Increment the pausedAt by the current time
+      setPausedAt((prev) => prev + audioContext.currentTime);
     } else {
-      audioContext.resume().then(() => {
-        console.log("Playback resumed successfully");
-        setIsPlaying(!isPlaying);
+      console.log("PLAY the track #", currentTrackNumber);
+      startAudioPlayback();
+    }
+    setIsPlaying(!isPlaying);
+  }
+
+  // Reset the pausedAt when the track changes
+  useEffect(() => {
+    setPausedAt(0);
+  }, [currentTrackNumber]);
+
+  function startAudioPlayback() {
+    if (isFirstPlaying) {
+      startingTime = Date.now();
+      audioNodes[0].start();
+      setIsFirstPlaying(false);
+    } else {
+      // For resuming, create a new source node, connect it to the destination,
+      // and add an onended event listener
+      const pausedNode = audioContext.createBufferSource();
+      pausedNode.buffer = audioBuffers[currentTrackNumber];
+      pausedNode.connect(audioContext.destination);
+      pausedNode.onended = () => {
+        console.log("track #", currentTrackNumber, "ended");
+        audioNodes[currentTrackNumber + 1].start(audioContext.currentTime);
+        setCurrentTrackNumber(currentTrackNumber + 1);
+      };
+      // Start the paused node at the pausedAt position
+      pausedNode.start(0, pausedAt);
+      // Update the old source node with the new source node
+      setAudioNodes((prev) => {
+        return prev.map((node, index) => {
+          if (index === currentTrackNumber) {
+            return pausedNode;
+          } else {
+            return node;
+          }
+        });
       });
     }
   }
